eff12931a33b14339bcafbf1249683c9880bda7b
Gabriel Simek <gabrielsimek@outlook.com>
2021-08-04 17:03:07 -0700

    React Router wired in

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import CharacterList from '../characters/CharacterList';
+import CharacterDetails from '../details/CharacterDetails';
+import { Switch, Route } from 'react-router-dom';
+import Header from '../header/Header';
+
+
 export default function App() {
-  return <CharacterList/>;
+  return (
+    <>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={CharacterList} />
+        <Route exact path="/:id" component={CharacterDetails}/>
+      </Switch>
+    </>
+  ); 
 }
---
+++ src/components/details/CharacterDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CharacterDetails = () => {
+  return <h1>Detail Page</h1>;
+};
+
+export default CharacterDetails;
